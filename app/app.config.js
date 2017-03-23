532f377862efa882de3131f838db6a417ddeb911
xuecat <1446969391@qq.com>
2017-03-24 00:13:05 +0800

    dda

--- app/app.config.js
+++ app/app.config.js
@@ -29,4 +29,16 @@ angular.module('app').config([
             }
         });
     }
-]);
+])
+.config(['$translateProvider', function($translateProvider) { //依赖js没写，明天加
+    $translateProvider.useSanitizeValueStrategy('escapeParameters');
+    $translateProvider.useCookieStorage();
+    $translateProvider.useStaticFilesLoader({
+        files: [{
+            prefix: '/translate/local-',
+            suffix: '.json'
+        },]
+    });
+    $translateProvider.preferredLanguage('cn');
+
+}]);
