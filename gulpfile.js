c213cf91d757471ff2f19f2a1415a1741273148a
xuecat <1446969391@qq.com>
2017-04-06 00:12:01 +0800

    4/6

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,75 @@
 var gulp = require('gulp'),
-    less = require('gulp-less');
+    less = require('gulp-less'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    minifycss = require('gulp-minify-css'),
+    jsonminify = require('gulp-jsonminify'),
+    htmlmin = require('gulp-htmlmin'),
+    clean = require('gulp-clean'),
+    rename = require('gulp-rename'),
+    watch = require('gulp-watch'),
+    replace = require('gulp-replace'),
+    runSequence = require('run-sequence');
 
-    gulp.task('testLess', function() {
-        gulp.src('src/less/index.less')
-            .pipe(less())
-            .pipe(gulp.dest('src/css'));
-    });
+var htmlSrc = ['app/**/*.html', '!app/lib/**/*.html', '!app/lib/*.html'];
+var jsSrc = ['app/**/*.js', '!app/lib/**/*.js', '!app/assets/**/*.js'];
+var cssSrc = ['app/assets/**/*.css', '!app/lib/**/*.css'];
 
-    gulp.task('default', ['testLess', 'elseTask']);
+var imgSrc = ['app/assets/img/*', 'app/assets/img/**/*'];
+
+var jsonSrc = ['app/translate/*.json', 'app/translate/**/*.json'];
+
+var libSrc = ['app/lib/**/*'];
+
+var buildPath = 'dist';
+
+gulp.task('createIndex', function() {});
+
+gulp.task('compressHTML', function() {
+    return gulp.src(htmlSrc)
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest(buildPath));
+});
+
+gulp.task('compressJS', function() {
+    return gulp.src(jsSrc)
+    .on('error', function(err) {//语法检查，打印错误
+        console.log('Error: ', err.message);
+    })
+    .pipe(uglify())//压缩
+    .pipe(gulp.dest(buildPath));
+});
+
+gulp.task('compressCSS', function() {
+    return gulp.src(cssSrc)
+    //.pipe(concat('index.min.css')) 为了使用oclazyload不整合
+    .pipe(minifycss())
+    .pipe(gulp.dest(buildPath + '/assets'));
+});
+
+gulp.task('compressJSON', function () {
+    return gulp.src(jsonSrc)
+        .pipe(jsonminify())
+        .pipe(gulp.dest(buildPath + 'translate'));
+});
+
+gulp.task('copyImg', function() {
+    return gulp.src(imgSrc)
+    .pipe(gulp.dest(buildPath + '/assets/img'));
+});
+
+gulp.task('copyLib', function() {
+    return gulp.src(libSrc)
+    .pipe(gulp.dest(buildPath + 'lib'));
+});
+
+gulp.task('clean', function() {
+    return gulp.src(buildPath)
+    .pipe(clean());
+});
+
+gulp.task('default', function() {
+    runSequence('createIndex', 
+    ['compressHTML', 'compressJS', 'compressCSS', 'compressJSON'],
+    ['copyLib', 'copyImg']);
+});
