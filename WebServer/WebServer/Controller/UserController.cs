205699c41d3060f7e00e0f00908f95386dc7154e
xuecat <1446969391@qq.com>
2017-04-19 21:45:06 +0800

    4/192

---
+++ WebServer/WebServer/Controller/UserController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using WebServer.Model;
+
+namespace WebServer.Controller
+{
+    public class UserController : ApiController
+    {
+        public List<Users> GetUser()
+        {
+            var userList = new List<Users> {
+            new Users{ Id=1,UName="hehe",UAge=12},
+            new Users{Id=2,UName="tete",UAge=23},
+            new Users{Id=3,UName="ll",UAge=34}
+            };
+
+            
+            return userList;
+        }
+    }
+}
---
+++ WebServer/WebServer/Model/Users.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebServer.Model
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string UName { get; set; }
+        public int UAge { get; set; }
+    }
+}
