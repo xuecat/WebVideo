0df49a9d51ff71edd5ab8a4b280e6e466cf1d91b
wangqiu <1446969391@qq.com>
2017-05-08 11:35:40 +0800

    5/8/1

--- WebServer/WebServer/WebConsoleServer/StartUp.cs
+++ WebServer/WebServer/WebConsoleServer/StartUp.cs
@@ -4,8 +4,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
 using System.Web.Http;
 using System.Web.Http.Dispatcher;
+using Microsoft.Owin.FileSystems;
+using Microsoft.Owin.StaticFiles;
 
 namespace WebConsoleServer
 {
@@ -29,7 +32,25 @@ namespace WebConsoleServer
                 defaults: new { id = RouteParameter.Optional }
             );
 
+            //////
+            System.Configuration.AppSettingsReader read = new AppSettingsReader();
+            string webpath = Convert.ToString(read.GetValue("Web.root", typeof(string)));
+            var physicalFileSystem = new PhysicalFileSystem(webpath);
+            var options = new FileServerOptions
+            {
+                EnableDefaultFiles = true,
+                FileSystem = physicalFileSystem
+            };
+            options.StaticFileOptions.FileSystem = physicalFileSystem;
+            options.StaticFileOptions.ServeUnknownFileTypes = true;
+            options.DefaultFilesOptions.DefaultFileNames = new[]
+            {
+                "index.html"
+            };
+            //////
+
             appBuilder.UseWebApi(config);
+            appBuilder.UseFileServer(options);
         }
     }
 }
