1bc0db56ab61e4a7fa4256b1a38208c06319da98
xuecat <1446969391@qq.com>
2017-05-18 23:05:19 +0800

    5/18

--- app/modules/home/home.js
+++ app/modules/home/home.js
@@ -1,74 +1,76 @@
 'user strict';
 
 angular.module('app')
-.controller('mainController', ['$scope', '$state','$translate', 'appToggleData', 'appAllConst', 'fileNavigator',
- function($scope, $state, $translate, appToggleData,appAllConst, FileNavigator) {
-     $scope.linkPage = $state.current.data;
-     
-     $scope.fileNavigator = new FileNavigator;
-     $scope.fileNavigator.createFileTree();
+    .controller('mainController', ['$scope', '$state', '$translate', 'appToggleData', 'appAllConst', 'fileNavigator',
+        function($scope, $state, $translate, appToggleData, appAllConst, FileNavigator) {
+            $scope.linkPage = $state.current.data;
 
-     $state.transitionTo('home.filemanager');
-    //  $state.go('home.video', {
-    //             type: 'mp4', 
-    //             param1: '',
-    //             param2: './data/test.mp4'
-    //         });
-    
-     $scope.$on('$stateChangeSuccess', 
-     function(event, toState, toParams, fromState, fromParams) {
-         if (toState) {
-            $scope.linkPage = toState.data;
-         }
-     });
+            $scope.fileNavigator = new FileNavigator;
+            $scope.fileNavigator.createFileTree();
 
-     $scope.selectTemp = null;
+            $state.transitionTo('home.filemanager');
+            //  $state.go('home.video', {
+            //             type: 'mp4', 
+            //             param1: '',
+            //             param2: './data/test.mp4'
+            //         });
 
-     $scope.isInThisPath = function(path) {
-        return path == $scope.fileNavigator.currentItem.name;
-     };
-     
-     $scope.isSelected = function(itm) {
-         if (itm) {
-             return $scope.selectTemp == itm;
-         }
-         return false;
-     }
+            $scope.$on('$stateChangeSuccess',
+                function(event, toState, toParams, fromState, fromParams) {
+                    if (toState) {
+                        $scope.linkPage = toState.data;
+                    }
+                });
 
-     $scope.selectOrUnselect = function(itm, $event) {
-         if (itm) {
-             $scope.selectTemp = itm;
-             if ($event)
-                $event.stopPropagation();
-         } else {
-             $scope.selectTemp = null;
-         }
-     }
+            $scope.selectTemp = null;
 
-    //  $scope.$on('$stateChangeStart', 
-    //  function(event, toState, toParams, fromState, fromParams) {
-    //      //event.preventDefault(); 
-    //  });
+            $scope.isInThisPath = function(path) {
+                if (!path) return false;
 
-    //  $scope.$on('$viewContentLoaded', function(event, a, b, c, d) {
-    //  });
-    // $scope.$on('$stateChangeStart', 
-    // function(event, toState, toParams, fromState, fromParams) {
-        //event.preventDefault(); 
-    //     if (fromState.name && fromState.name != "home" && toState.name == "home.dashboard") {
-    //         appToggleData.toggleDashboardView = 'anim-slide-right';
-    //         appToggleData.toggleTablesView = 'anim-slide-left';
-    //         appToggleData.toggleVideoView = 'anim-slide-left';
-    //     } else if (fromState.name == "home.dashboard") {
-    //         appToggleData.toggleTablesView = 'anim-slide-left';
-    //         appToggleData.toggleDashboardView = 'anim-slide-right';
-    //     }
-    // });
+                return path === $scope.fileNavigator.currentItem.name;
+            };
 
-     $scope.toggle = true;
+            $scope.isSelected = function(itm) {
+                if (itm) {
+                    return $scope.selectTemp == itm;
+                }
+                return false;
+            }
 
-     $scope.ToggleSidebar = function() {
-         $scope.toggle = !$scope.toggle;
-     }
- }
-]);
+            $scope.selectOrUnselect = function(itm, $event) {
+                if (itm) {
+                    $scope.selectTemp = itm;
+                    if ($event)
+                        $event.stopPropagation();
+                } else {
+                    $scope.selectTemp = null;
+                }
+            }
+
+            //  $scope.$on('$stateChangeStart', 
+            //  function(event, toState, toParams, fromState, fromParams) {
+            //      //event.preventDefault(); 
+            //  });
+
+            //  $scope.$on('$viewContentLoaded', function(event, a, b, c, d) {
+            //  });
+            // $scope.$on('$stateChangeStart', 
+            // function(event, toState, toParams, fromState, fromParams) {
+            //event.preventDefault(); 
+            //     if (fromState.name && fromState.name != "home" && toState.name == "home.dashboard") {
+            //         appToggleData.toggleDashboardView = 'anim-slide-right';
+            //         appToggleData.toggleTablesView = 'anim-slide-left';
+            //         appToggleData.toggleVideoView = 'anim-slide-left';
+            //     } else if (fromState.name == "home.dashboard") {
+            //         appToggleData.toggleTablesView = 'anim-slide-left';
+            //         appToggleData.toggleDashboardView = 'anim-slide-right';
+            //     }
+            // });
+
+            $scope.toggle = true;
+
+            $scope.ToggleSidebar = function() {
+                $scope.toggle = !$scope.toggle;
+            }
+        }
+    ]);
